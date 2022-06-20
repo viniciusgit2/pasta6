2c557e5c79e1fda33aa9660bcb8e2e2eff8bf55d
viniciusgit2 <4xv6patelfin@gmail.com>
2022-06-20 11:25:54 -0300

    novo modulo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+import './index.css';
+
+import primeiro from"./src./componentes/primeiro";
+
+ReactDOM.render( document.getElementByIdroot(primeiro))
+ 
+
+
